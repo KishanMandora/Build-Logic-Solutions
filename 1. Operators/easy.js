d89eb87263f636bb4480a89c39c5248441cb2ed6
Kishan Mandora <kishanmandora7@gmail.com>
2021-10-18 17:47:22 +0530

    add half easy operator exercises

---
+++ 1. Operators/easy.js
@@ -0,0 +1,14 @@
+// program to  add 5 numbers
+
+const addFiveNumbers = (n1, n2, n3, n4, n5) => n1 + n2 + n3 + n4 + n5;
+
+// program to find if number is odd or even
+
+const oddOrEven = (num) => (num % 2 === 0 ? `${num} is even` : `${num} is odd`);
+
+// program to find maximum out of two numbers
+
+const maxOfTwo = (n1, n2) =>
+	n1 > n2
+		? `number1 is greater than number2`
+		: `number2 is greater than number1`;
