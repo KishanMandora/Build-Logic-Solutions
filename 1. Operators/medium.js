ed07ea0fac0660e550ce6d6ae1b8d1998a9b1b02
Kishan Mandora <kishanmandora7@gmail.com>
2021-10-21 15:14:05 +0530

    add two medium operator exercises

--- 1. Operators/medium.js
+++ 1. Operators/medium.js
@@ -28,3 +28,40 @@ const multiplicationTable = (n) => {
 		console.log(`${n} * ${i} = ${n * i}`);
 	}
 };
+
+// fibonacci
+
+// factorial
+
+const factorial = (num) => {
+	let factorial = 1;
+	for (let i = 1; i <= num; i++) {
+		factorial = factorial * i;
+	}
+	return factorial;
+};
+
+// program to find if no is prime or not
+
+// weekend or weekday
+
+const dayChecker = (day) => {
+	switch (day.toLowerCase()) {
+		case "monday":
+			return `${day}  is a weekday`;
+		case "tuesday":
+			return `${day}  is a weekday`;
+		case "wednesday":
+			return `${day}  is a weekday`;
+		case "thursday":
+			return `${day}  is a weekday`;
+		case "friday":
+			return `${day}  is a weekday`;
+		case "saturday":
+			return `${day}  is a weekend`;
+		case "sunday":
+			return `${day}  is a weekend`;
+		default:
+			return `${day} doesn't exist, please enter a valid day`;
+	}
+};
