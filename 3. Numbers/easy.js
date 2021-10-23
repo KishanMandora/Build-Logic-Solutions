5fb75d06348d64bdb3d1405f41d716782bfb2553
Kishan Mandora <kishanmandora7@gmail.com>
2021-10-23 21:31:52 +0530

    add three easy  numbers exercise

---
+++ 3. Numbers/easy.js
@@ -0,0 +1,12 @@
+// sum of two number
+
+const sumOfTwo = (num1, num2) => console.log(num1 + num2);
+
+// Simple Interest
+
+const simpleInterest = (p, r, t) => `Simple interest is ${(p * r * t) / 100}`;
+
+// kinetic energy
+
+const kineticEnergy = (mass, velocity) =>
+	`Kinetic energy is ${0.5 * mass * velocity * velocity}`;
