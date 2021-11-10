565cf611ac19f39acfd40e78fb1fcb8a6d892dd9
Kishan Mandora <kishanmandora7@gmail.com>
2021-11-10 06:13:46 +0530

    add 3 of easy functions exercises

--- 2. Functions/easy.js
+++ 2. Functions/easy.js
@@ -7,25 +7,27 @@ const areaOfHexagon = (a) => (3 * Math.sqrt(3) * a * a) / 2;
 
 // no of words
 
+const noOfWords = (str) => str.split(" ").length;
+
 // minimum of all numbers
 const findMin = (...args) =>
-	args.reduce((prev, current) => (prev < current ? prev : current));
+  args.reduce((prev, current) => (prev < current ? prev : current));
 
 // maximum of all numbers
 const findMax = (...args) =>
-	args.reduce((prev, current) => (prev > current ? prev : current));
+  args.reduce((prev, current) => (prev > current ? prev : current));
 
 // type of triangle
 
 const typeOfTriangle = (side1, side2, side3) => {
-	if (side1 + side2 + side3 !== 180)
-		return "The sum of all side should be 180 degree";
-	else {
-		if (side1 === side2 && side2 === side3 && side1 === side3)
-			return "It is an Equilateral Triangle";
-		else if (side1 === side2 || side2 === side3 || side3 === side1)
-			return "It is an Isoceles Triangle";
-		else if (side1 !== side2 && side1 !== side3 && side3 !== side2)
-			return "It is a scalene triangle";
-	}
+  if (side1 + side2 + side3 !== 180)
+    return "The sum of all side should be 180 degree";
+  else {
+    if (side1 === side2 && side2 === side3 && side1 === side3)
+      return "It is an Equilateral Triangle";
+    else if (side1 === side2 || side2 === side3 || side3 === side1)
+      return "It is an Isoceles Triangle";
+    else if (side1 !== side2 && side1 !== side3 && side3 !== side2)
+      return "It is a scalene triangle";
+  }
 };
