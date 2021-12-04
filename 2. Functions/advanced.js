a62d8df54a258e7890c443b7aef8ffde84296112
Kishan Mandora <kishanmandora7@gmail.com>
2021-12-04 19:47:00 +0530

    add 2nd of advanced functions exercises

--- 2. Functions/advanced.js
+++ 2. Functions/advanced.js
@@ -10,3 +10,32 @@ const encodeString = (str, shiftBy) => {
 
   return newStr;
 };
+
+// sentence case
+
+const toSentenceCase = (str) => {
+  let spaceCheck = false;
+  let sentenceCaseString = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const characterCode = str.charCodeAt(i);
+
+    if (i === 0) {
+      sentenceCaseString =
+        sentenceCaseString + String.fromCharCode(characterCode - 32);
+    } else if (str[i] === " ") {
+      sentenceCaseString =
+        sentenceCaseString + String.fromCharCode(characterCode);
+      spaceCheck = true;
+    } else if (spaceCheck) {
+      sentenceCaseString =
+        sentenceCaseString + String.fromCharCode(characterCode - 32);
+      spaceCheck = false;
+    } else {
+      sentenceCaseString =
+        sentenceCaseString + String.fromCharCode(characterCode);
+    }
+  }
+
+  return sentenceCaseString;
+};
